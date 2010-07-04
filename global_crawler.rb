d27f7c2c1d60888d9f3568ff20ff3ab72f3bbc1b
Matt Gornick <mattgornick@gmail.com>
2010-07-04 09:32:55 -0500

    beginning to integrate the runner code

--- global_crawler.rb
+++ global_crawler.rb
@@ -4,18 +4,18 @@ require 'capybara/dsl'
 
 class GlobalCrawler
   
-  def search(term)    
+  def load_file(filename)
+    return IO.read('trademarks.txt').split("\n")
+  end
+  
+  def search_google(term)    
     Capybara.current_driver = :culerity
     Capybara.app_host = 'http://www.google.com'
     Capybara.visit('/')
     Capybara.fill_in "q", :with => 'Honda'
     Capybara.click 'Google Search'
-    # puts Capybara.page.body
-    self.google(Capybara.page.body.to_s)
+    GoogleCrawler.new(Capybara.page.body.to_s)
   end
-  
-  def google(page)
-    GoogleCrawler.new(page)
-  end
+
 end
 
--- runner.rb
+++ runner.rb
@@ -1,18 +1,20 @@
 require 'global_crawler'
 require 'ap'
 
-c = GlobalCrawler.new
-g = c.search('honda')
+puts ARGV.first
 
-puts "Organic Search Results\n"
-puts "-------------------------"
-g.organic_results(10).each do |link|
-  puts link
-end
-
-puts "\n"
-puts "Sponsored Search Results\n"
-puts "-------------------------"
-g.sponsored_results(10).each do |link|
-  puts link
-end
+# c = GlobalCrawler.new
+# g = c.search_google('honda')
+# 
+# puts "Organic Search Results\n"
+# puts "-------------------------"
+# g.organic_results(10).each do |link|
+#   puts link
+# end
+# 
+# puts "\n"
+# puts "Sponsored Search Results\n"
+# puts "-------------------------"
+# g.sponsored_results(10).each do |link|
+#   puts link
+# end
