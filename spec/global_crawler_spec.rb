b7dfd16005ef33f5222ae39cc0c38ff92181a494
Matt Gornick <mattgornick@gmail.com>
2010-07-27 09:56:43 -0500

    added new tests for alphanumeric characters

--- spec/global_crawler_spec.rb
+++ spec/global_crawler_spec.rb
@@ -3,6 +3,6 @@ require File.expand_path(File.join(File.dirname(__FILE__),'..','global_crawler')
 describe "GlobalCrawler" do
   it "should only include alphanumeric characters in the trademark" do
     GlobalCrawler.clean_trademark("AbCD EFG 123").should == "AbCDEFG123"
-    GlobalCrawler.clean_trademark("a!@$%^&*(),./?~`b").should == "ab"
+    GlobalCrawler.clean_trademark("a!@$%^&*(),./?~`b\\").should == "ab"
   end
 end
