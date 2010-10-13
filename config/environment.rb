60646cbd1c7432e4fadd151967186de0cd0aaa2d
Matt Gornick <mattgornick@gmail.com>
2010-10-13 12:25:41 -0500

    working on new version of trademarks

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -16,15 +16,17 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   config.gem "nokogiri"
+  config.gem 'capybara'
+  config.gem 'culerity'
+  config.gem 'will_paginate'
+  
   require 'google_crawler'
   require 'bing_crawler'
   require 'yahoo_crawler'
-  config.gem 'capybara'
-  config.gem 'pdfkit'
-  # config.gem 'capybara/dsl'
+  require 'pdfkit'
+  config.middleware.use PDFKit::Middleware, :print_media_type => true
   require 'net/http'
   require 'fileutils'
-  config.gem 'will_paginate', :version => '~> 2.3.11', :source => 'http://gemcutter.org'
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
