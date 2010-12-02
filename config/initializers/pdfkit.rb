bba551ed7447b5e3e5931239f8c3432bb09797ef
Matt Gornick <mattgornick@gmail.com>
2010-12-02 12:14:51 -0600

    removed ignore_load_errors flag

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -2,7 +2,7 @@
 PDFKit.configure do |config|
   config.wkhtmltopdf = Rails.root.join('vendor', 'wkhtmltopdf-amd64').to_s if RAILS_ENV == 'production'
 
-  config.default_options = {
-      :ignore_load_errors => true
-    }
+  # config.default_options = {
+  #     :ignore_load_errors => true
+  #   }
 end
